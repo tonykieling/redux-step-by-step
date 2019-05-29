5172a08a09717991c3f08f9958abdf07a4e2d195
Tony Kieling <tony.kieling@gmail.com>
2019-05-28 18:47:44 -0700

    doing store outside index.js

--- v3/src/Counter.js
+++ v3/src/Counter.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
+const store = require('./index.js')
 
 class Counter extends Component {
   render() {
     return (
       <div>
-        Number: <span> { this.props.number } </span> <br /> <br />
+        {/* Number: <span> { this.props.number } </span> <br /> <br /> */}
+        Number: <span> { this.store.getState() } </span> <br /> <br />
       </div>
     );
   }
 }
 
-const mapStateToProps = store => {
-  return {
-    number: store.number
-  }
-}
+// todo
+// NEED TO CREATE store in a place, call it inside index.js and here in order to access getState method
 
-export default connect(mapStateToProps, null)(Counter);
+// const mapStateToProps = store => {
+//   return {
+//     number: store.number
+//   }
+// }
+
+// export default connect(mapStateToProps, null)(Counter);
+export default Counter;
